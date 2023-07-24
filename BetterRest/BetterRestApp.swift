933190bf572f2ad883d21884ef98c12086aaa75f
Иван Лясковец <lyaskovets.iv@MacBook-Air-Ivan.local>
2023-07-24 23:22:23 +0300

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Иван Лясковец on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
