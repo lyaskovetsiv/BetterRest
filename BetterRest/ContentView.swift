52663fbe61f16c1f60f932a31a2cc278022674c9
Иван Лясковец <lyaskovets.iv@MacBook-Air-Ivan.local>
2023-07-26 21:12:11 +0300

    Refactored UI

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -13,36 +13,49 @@ struct ContentView: View {
 	
 	// MARK: - States
 	
-	@State private var wakeUp: Date = Date.now
+	@State private var wakeUp = defaultWakedTime
 	@State private var sleepAmount = 8.0
 	@State private var coffeeAmount = 1
 	@State private var alertTitle = ""
 	@State private var alertMessage = ""
 	@State private var showingAlert = false
 	
+	static var defaultWakedTime: Date {
+		var components = DateComponents()
+		components.hour = 7
+		components.minute = 0
+		return Calendar.current.date(from: components) ?? Date.now
+	}
+	
     var body: some View {
 		NavigationView {
-			VStack {
-				Text("When do you want to wake up?")
-					.font(.headline)
-				DatePicker("Please enter a time",
-						   selection: $wakeUp,
-						   displayedComponents: DatePickerComponents.hourAndMinute)
+			Form {
+				VStack (alignment: .leading, spacing: 0) {
+					Text("When do you want to wake up?")
+						.font(.headline)
+					DatePicker("Please enter a time",
+							   selection: $wakeUp,
+							   displayedComponents: DatePickerComponents.hourAndMinute)
 					.labelsHidden()
+				}
 				
-				Text("Desired amount of sleep")
-					.font(.headline)
-				Stepper("\(sleepAmount.formatted()) hours",
-						value: $sleepAmount,
-						in: 4...12,
-						step: 0.25)
+				VStack(alignment: .leading, spacing: 0) {
+					Text("Desired amount of sleep")
+						.font(.headline)
+					Stepper("\(sleepAmount.formatted()) hours",
+							value: $sleepAmount,
+							in: 4...12,
+							step: 0.25)
+				}
 				
-				Text("Daily coffee intake")
-					.font(.headline)
-				Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
-						value: $coffeeAmount,
-						in: 1...20,
-						step: 1)
+				VStack(alignment: .leading, spacing: 0) {
+					Text("Daily coffee intake")
+						.font(.headline)
+					Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+							value: $coffeeAmount,
+							in: 1...20,
+							step: 1)
+				}
 			}
 			.navigationTitle("BetterRest")
 			.toolbar {
