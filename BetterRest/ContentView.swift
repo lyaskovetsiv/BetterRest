2cf2dd8c8576e2de7ccf4de089e4ae9a5b50ff38
Иван Лясковец <lyaskovets.iv@MacBook-Air-Ivan.local>
2023-07-26 20:26:35 +0300

    Added UI on main screen

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -13,16 +13,33 @@ struct ContentView: View {
 	
 	@State private var wakeUp: Date = Date.now
 	@State private var sleepAmount = 8.0
-	@State private var coffeAmount = 1
+	@State private var coffeeAmount = 1
 	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationView {
+			VStack {
+				Text("When do you want to wake up?")
+					.font(.headline)
+				DatePicker("Please enter a time",
+						   selection: $wakeUp,
+						   displayedComponents: DatePickerComponents.hourAndMinute)
+					.labelsHidden()
+				
+				Text("Desired amount of sleep")
+					.font(.headline)
+				Stepper("\(sleepAmount.formatted()) hours",
+						value: $sleepAmount,
+						in: 4...12,
+						step: 0.25)
+				
+				Text("Daily coffee intake")
+					.font(.headline)
+				Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+						value: $coffeeAmount,
+						in: 1...20,
+						step: 1)
+			}
+		}
     }
 }
 
